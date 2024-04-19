f1098e227411ca35428c0d42ed20ef566fd3d6f5
Ajitesh <ajitesh@deloitte.com>
2024-04-19 19:18:47 +0530

    Bitwise Complement Operator Changes

--- 6. UnaryOperators/src/example.java
+++ 6. UnaryOperators/src/example.java
@@ -46,5 +46,23 @@ public class example {
         //result = (prefix d) + (postfix d) + (prefix d) + (prefix d) + (postfix d)
         System.out.println("Final value of d: " + d);
         System.out.println("result : " + result);
+        System.out.println();
+
+        //Bitwise Complement Operator
+        //It is basically 2's complement for an integer
+        int e = 2;
+        e = ~e;
+        //Bitwise Complement is basically 2's complement for an integer
+        //Formula for positive operand : ~(operand) = -(operand+1)
+        //binary of 2 is 10. So, -(10+1) will be bitwise complement of 2 i.e. -(11) in binary
+        //on converting -(11) to decimal it will be -(3)
+        System.out.println("Bitwise Complement Operator ~(operand), e : "+e);
+
+        int f = -10;
+        f = ~f;
+        //Formula for negative operand : ~(operand) = (operand-1)
+        //binary of -10 is -(1010). So, (1010-1) will be bitwise complement of 2 i.e. (1001) in binary
+        //on converting (1001) to decimal it will be 9
+        System.out.println("Bitwise Complement Operator ~(operand), f : "+f);
     }
 }
