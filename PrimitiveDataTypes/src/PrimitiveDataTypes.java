c52c8f2aa486748db6ce16af1e824834fffa0ba3
Ajitesh <ajitesh@deloitte.com>
2024-04-19 12:26:35 +0530

    added boolean data type variable.

--- PrimitiveDataTypes/src/PrimitiveDataTypes.java
+++ PrimitiveDataTypes/src/PrimitiveDataTypes.java
@@ -7,6 +7,7 @@ public class PrimitiveDataTypes {
         double Double=45.555666664667777;
         float Float=10.5F;
         long Long=999999999;
+        boolean bool = true;
         System.out.println("character:"+ch);
         System.out.println("Integer:"+integer);
         System.out.println("byte:"+Byte);
@@ -14,5 +15,6 @@ public class PrimitiveDataTypes {
         System.out.println("double: "+Double);
         System.out.println("float: "+Float);
         System.out.println("long: "+Long);
+        System.out.println("boolean: "+bool);
     }
 }
