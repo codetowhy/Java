b22dc0417e0672b73550a16c55585a5173d13c86
Guest.Win11 <Guest.Win11@outlook.com>
2024-08-24 22:30:30 +0530

    Formatted Output in Java

---
+++ 0013. FormattedOutput/src/example.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args)
+    {
+        double a = 3.14159265359;
+
+        System.out.printf("%f\n", a);
+        System.out.printf("%5.3f\n", a);
+        System.out.printf("%5.2f\n", a);
+    }
+}
